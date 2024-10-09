69e67aab640c83fe81230cf05cf17d8b8ff550fa
Ian Kabaka <iankabaka1@gmail.com>
2024-10-09 14:16:05 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: userName, email: userEmail }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector("body").textContent = data.id;
+    })
+    .catch((error) => {
+      document.querySelector("body").textContent = error.message;
+    });
+}
